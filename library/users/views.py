a8c6c47aeabdf86a5de6bef0d5f65f0576801350
harrisdurrani <harris.durrani@hotmail.com>
2021-05-31 13:26:23 -0700

    added contacts form

--- library/users/views.py
+++ library/users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, ContactForm
+from django.core.mail import send_mail, BadHeaderError
 
 def register(request):
     if request.method == "POST":
@@ -14,3 +15,23 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
+
+
+def contactView(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email, ['harrisdurrani1@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('success')
+    return render(request, "users/contact.html", {'form': form})
+
+def successView(request):
+    return HttpResponse('Success! Thank you for your message.')
--- library/website/urls.py
+++ library/website/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from users import views as view_contact
 
 
 urlpatterns = [
     path('', views.home, name='website-home'),
     path('about/', views.about, name='website-about'),
     path('catalog/', views.catalog, name='website-catalog'),
+    path('contact/', view_contact.contactView, name='users-contact')
 ]
