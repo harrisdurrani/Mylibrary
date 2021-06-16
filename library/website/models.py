6a01ba0b22c0cec3a9aea7be15fb122c45917fac
harrisdurrani <harris.durrani@hotmail.com>
2021-06-15 18:31:31 -0700

    singlehandedly added library card creation trackin

--- library/website/models.py
+++ library/website/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User as user_model
+
 
 class LibraryCardModel(models.Model):
 	user_email = models.CharField(max_length=40)
 	first_name = models.CharField(max_length=40)
 	last_name = models.CharField(max_length=40)
+	library_card_id = models.AutoField(primary_key=True)
+	owner = models.OneToOneField(user_model, on_delete=models.CASCADE, null=True, default=None)
 
+	def __str__(self):
+		return str(self.library_card_id)
--- library/website/views.py
+++ library/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import LibraryCardForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -17,9 +18,10 @@ def library_card_view(request):
     if request.method == "POST":
         form = LibraryCardForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            # messages.success(request, f'Account created!')
+            library_card = form.save(commit=False)
+            library_card.owner = request.user
+            library_card.save()
+            messages.success(request, f'Card created! {library_card}')
             return redirect('/')
     else:
         form = LibraryCardForm()
